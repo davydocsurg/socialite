cecb0fc7c8da3b8282c4f09097934bdef024a29f
davydocsurg <davydocsurg@gmail.com>
2022-07-11 02:39:05 +0100

    update tweet icons

--- src/components/tweets/AuthUserTweet.jsx
+++ src/components/tweets/AuthUserTweet.jsx
@@ -1,125 +1,135 @@
 import React, { forwardRef, useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
-import RepeatIcon from "@material-ui/icons/Repeat";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import PublishIcon from "@material-ui/icons/Publish";
+import {
+    Like,
+    Retweet,
+    Comment,
+    Share,
+} from "../../utils/baseIcons/tweetCompIcons";
 import moment from "moment";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import {
-  LikeTweet,
-  UnlikeTweet,
-  FetchTweetsAction,
-  RefreshTweetsAction,
-  RefreshAuthUser,
+    LikeTweet,
+    UnlikeTweet,
+    FetchTweetsAction,
+    RefreshTweetsAction,
+    RefreshAuthUser,
 } from "../../redux/actions/TweetActions";
 
 const AuthUserTweet = forwardRef(
-  (
-    {
-      tweepName,
-      slug,
-      username,
-      verified,
-      text,
-      tweetImage,
-      avatar,
-      tweetTime,
-      tweepLikeId,
-      likesCount,
-      authUserId,
-    },
-    ref
-  ) => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const token = localStorage.getItem("user-token");
-    const [showLikedBtn, setShowLikedBtn] = useState(false);
+    (
+        {
+            tweepName,
+            slug,
+            username,
+            verified,
+            text,
+            tweetImage,
+            avatar,
+            tweetTime,
+            tweepLikeId,
+            likesCount,
+            authUserId,
+        },
+        ref
+    ) => {
+        const history = useHistory();
+        const dispatch = useDispatch();
+        const token = localStorage.getItem("user-token");
+        const [showLikedBtn, setShowLikedBtn] = useState(false);
 
-    useEffect(() => {
-      checkLikes();
-    }, []);
+        useEffect(() => {
+            checkLikes();
+        }, []);
 
-    const checkLikes = () => {
-      const compAuthId = (value, index, array) => {
-        value !== authUserId ? setShowLikedBtn(false) : setShowLikedBtn(true);
-      };
-      let ff = tweepLikeId.filter(compAuthId);
-      return ff;
-    };
+        const checkLikes = () => {
+            const compAuthId = (value, index, array) => {
+                value !== authUserId
+                    ? setShowLikedBtn(false)
+                    : setShowLikedBtn(true);
+            };
+            let ff = tweepLikeId.filter(compAuthId);
+            return ff;
+        };
 
-    const viewTweet = () => {
-      history.push(`/tweet/${slug}`);
-    };
+        const viewTweet = () => {
+            history.push(`/tweet/${slug}`);
+        };
 
-    const handleLikeTweet = () => {
-      dispatch(LikeTweet(slug));
-      setTimeout(() => {
-        setShowLikedBtn(true);
-        dispatch(RefreshAuthUser());
-      }, 500);
-    };
+        const handleLikeTweet = () => {
+            dispatch(LikeTweet(slug));
+            setTimeout(() => {
+                setShowLikedBtn(true);
+                dispatch(RefreshAuthUser());
+            }, 500);
+        };
 
-    const handleUnlikeTweet = () => {
-      dispatch(UnlikeTweet(slug));
-      setTimeout(() => {
-        setShowLikedBtn(false);
-        dispatch(RefreshAuthUser());
-      }, 500);
-    };
+        const handleUnlikeTweet = () => {
+            dispatch(UnlikeTweet(slug));
+            setTimeout(() => {
+                setShowLikedBtn(false);
+                dispatch(RefreshAuthUser());
+            }, 500);
+        };
 
-    return (
-      <div className="post" ref={ref}>
-        <div className="post__avatar">
-          <Avatar src={avatar} className="shadow-lg" />
-        </div>
-        <div className="post__body">
-          <div className="post__header">
-            <div className="post__headerText row d-flex">
-              <h3 className="col-lg-9 col-md-8">
-                {tweepName}{" "}
-                <span className="post__headerSpecial">
-                  {verified && <VerifiedUserIcon className="post__badge" />} @
-                  {username}
-                </span>
-              </h3>
+        return (
+            <div className="post" ref={ref}>
+                <div className="post__avatar">
+                    <Avatar src={avatar} className="shadow-lg" />
+                </div>
+                <div className="post__body">
+                    <div className="post__header">
+                        <div className="post__headerText row d-flex">
+                            <h3 className="col-lg-9 col-md-8">
+                                {tweepName}{" "}
+                                <span className="post__headerSpecial">
+                                    {verified && (
+                                        <VerifiedUserIcon className="post__badge" />
+                                    )}{" "}
+                                    @{username}
+                                </span>
+                            </h3>
 
-              <div className="tweet_time col-lg-3 col-md-4">
-                <b>{moment(tweetTime).fromNow()}</b>
-              </div>
+                            <div className="tweet_time col-lg-3 col-md-4">
+                                <b>{moment(tweetTime).fromNow()}</b>
+                            </div>
+                        </div>
+                        <div className="post__headerDescription">
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                    {tweetImage ? (
+                        <img
+                            src={tweetImage}
+                            alt=""
+                            className="py-2 img-fluid"
+                        />
+                    ) : null}
+                    <div className="post__footer">
+                        <Comment fontSize="small" />
+                        <Retweet fontSize="small" />
+                        <div className="">
+                            {showLikedBtn ? (
+                                <i
+                                    className="fas fa-heart text-danger"
+                                    onClick={handleUnlikeTweet}
+                                ></i>
+                            ) : (
+                                <Like
+                                    fontSize="small"
+                                    onClick={handleLikeTweet}
+                                />
+                            )}{" "}
+                            {likesCount > 0 && likesCount}
+                        </div>{" "}
+                        <Share fontSize="small" />
+                    </div>
+                </div>
             </div>
-            <div className="post__headerDescription">
-              <p>{text}</p>
-            </div>
-          </div>
-          {tweetImage ? (
-            <img src={tweetImage} alt="" className="py-2 img-fluid" />
-          ) : null}
-          <div className="post__footer">
-            <ChatBubbleOutlineIcon fontSize="small" />
-            <RepeatIcon fontSize="small" />
-            <div className="">
-              {showLikedBtn ? (
-                <i
-                  className="fas fa-heart text-danger"
-                  onClick={handleUnlikeTweet}
-                ></i>
-              ) : (
-                <FavoriteBorderIcon
-                  fontSize="small"
-                  onClick={handleLikeTweet}
-                />
-              )}{" "}
-              {likesCount > 0 && likesCount}
-            </div>{" "}
-            <PublishIcon fontSize="small" />
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 );
 
 export default AuthUserTweet;
--- src/components/tweets/Tweet.jsx
+++ src/components/tweets/Tweet.jsx
@@ -1,159 +1,170 @@
 import React, { createRef, forwardRef, useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
-import RepeatIcon from "@material-ui/icons/Repeat";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import {
+    Retweet,
+    Like,
+    Share,
+    Comment,
+} from "../../utils/baseIcons/tweetCompIcons";
 import PublishIcon from "@material-ui/icons/Publish";
 import moment from "moment";
 import { Link, useHistory } from "react-router-dom";
 import { useDispatch, connect } from "react-redux";
 import PropTypes from "prop-types";
 import {
-  LikeTweet,
-  UnlikeTweet,
-  FetchTweetsAction,
-  RefreshTweetsAction,
-  FetchTweet,
+    LikeTweet,
+    UnlikeTweet,
+    FetchTweetsAction,
+    RefreshTweetsAction,
+    FetchTweet,
 } from "../../redux/actions/TweetActions";
 
 const Tweet = forwardRef(
-  (
-    {
-      tweepName,
-      username,
-      verified,
-      text,
-      tweetImage,
-      avatar,
-      tweetTime,
-      likesCount,
-      tweepLikeId,
-      authUserId,
-      slug,
-    },
-    ref
-  ) => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const token = localStorage.getItem("user-token");
-    const [showLikedBtn, setShowLikedBtn] = useState(false);
-    const tweetRef = createRef();
+    (
+        {
+            tweepName,
+            username,
+            verified,
+            text,
+            tweetImage,
+            avatar,
+            tweetTime,
+            likesCount,
+            tweepLikeId,
+            authUserId,
+            slug,
+        },
+        ref
+    ) => {
+        const history = useHistory();
+        const dispatch = useDispatch();
+        const token = localStorage.getItem("user-token");
+        const [showLikedBtn, setShowLikedBtn] = useState(false);
+        const tweetRef = createRef();
 
-    useEffect(() => {
-      // fetchTweet();
-      checkLikes();
-    }, [likesCount]);
+        useEffect(() => {
+            // fetchTweet();
+            checkLikes();
+        }, [likesCount]);
 
-    // const user = useSelector((state) => state.user.credentials);
-    // const allTweets = useSelector((state) => state.tweetReducer.allTweets);
+        // const user = useSelector((state) => state.user.credentials);
+        // const allTweets = useSelector((state) => state.tweetReducer.allTweets);
 
-    const checkLikes = () => {
-      if (likesCount < 1) {
-        setShowLikedBtn(false);
-      }
-      const compAuthId = (value, index, array) => {
-        value !== authUserId ? setShowLikedBtn(false) : setShowLikedBtn(true);
-      };
-      let ff = tweepLikeId.filter(compAuthId);
+        const checkLikes = () => {
+            if (likesCount < 1) {
+                setShowLikedBtn(false);
+            }
+            const compAuthId = (value, index, array) => {
+                value !== authUserId
+                    ? setShowLikedBtn(false)
+                    : setShowLikedBtn(true);
+            };
+            let ff = tweepLikeId.filter(compAuthId);
 
-      return ff;
-    };
+            return ff;
+        };
 
-    const fetchTweet = () => {
-      dispatch(fetchTweet());
-    };
+        const fetchTweet = () => {
+            dispatch(fetchTweet());
+        };
 
-    const viewTweet = () => {
-      // dispatch(FetchTweet(slug));
+        const viewTweet = () => {
+            // dispatch(FetchTweet(slug));
 
-      history.push(`/tweet/${slug}`);
-      // setTimeout(() => {
-      // }, 200);
-    };
+            history.push(`/tweet/${slug}`);
+            // setTimeout(() => {
+            // }, 200);
+        };
 
-    const handleLikeTweet = () => {
-      dispatch(LikeTweet(slug));
-      setTimeout(() => {
-        setShowLikedBtn(true);
-        checkLikes();
-        dispatch(RefreshTweetsAction());
-      }, 500);
-    };
+        const handleLikeTweet = () => {
+            dispatch(LikeTweet(slug));
+            setTimeout(() => {
+                setShowLikedBtn(true);
+                checkLikes();
+                dispatch(RefreshTweetsAction());
+            }, 500);
+        };
 
-    const handleUnlikeTweet = () => {
-      dispatch(UnlikeTweet(slug));
-      setTimeout(() => {
-        setShowLikedBtn(false);
-        checkLikes();
-        dispatch(RefreshTweetsAction());
-      }, 500);
-    };
+        const handleUnlikeTweet = () => {
+            dispatch(UnlikeTweet(slug));
+            setTimeout(() => {
+                setShowLikedBtn(false);
+                checkLikes();
+                dispatch(RefreshTweetsAction());
+            }, 500);
+        };
 
-    return (
-      <div className="post" ref={tweetRef}>
-        <div className="post__avatar">
-          <Avatar src={avatar} className="shadow-lg" />
-        </div>
-        <div className="post__body">
-          <div className="post__header">
-            <div className="post__headerText row d-flex">
-              <h3 className="col-lg-9 col-md-8">
-                {tweepName}{" "}
-                <span className="post__headerSpecial">
-                  {verified ? (
-                    <small
-                      className="fas fa-check-circle text-twitter-color ml-5"
-                      data-toggle="tooltip"
-                      data-placement="right"
-                      title="Verified"
-                      data-fa-transform="shrink-4 down-2"
-                    ></small>
-                  ) : null}{" "}
-                  @{username}
-                </span>
-              </h3>
+        return (
+            <div className="post" ref={tweetRef}>
+                <div className="post__avatar">
+                    <Avatar src={avatar} className="shadow-lg" />
+                </div>
+                <div className="post__body">
+                    <div className="post__header">
+                        <div className="post__headerText row d-flex">
+                            <h3 className="col-lg-9 col-md-8">
+                                {tweepName}{" "}
+                                <span className="post__headerSpecial">
+                                    {verified ? (
+                                        <small
+                                            className="fas fa-check-circle text-twitter-color ml-5"
+                                            data-toggle="tooltip"
+                                            data-placement="right"
+                                            title="Verified"
+                                            data-fa-transform="shrink-4 down-2"
+                                        ></small>
+                                    ) : null}{" "}
+                                    @{username}
+                                </span>
+                            </h3>
 
-              <div className="tweet_time col-lg-3 col-md-4">
-                <b>{moment(tweetTime).fromNow()}</b>
-              </div>
+                            <div className="tweet_time col-lg-3 col-md-4">
+                                <b>{moment(tweetTime).fromNow()}</b>
+                            </div>
+                        </div>
+                        {/* <Link to={slug}> */}
+                        <div
+                            className="post__headerDescription"
+                            onClick={viewTweet}
+                        >
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                    {tweetImage ? (
+                        <img
+                            src={tweetImage}
+                            alt=""
+                            className="py-2 img-fluid"
+                        />
+                    ) : null}
+
+                    {/* </Link> */}
+                    {token && (
+                        <div className="post__footer">
+                            <Comment />
+                            <Retweet fontSize="small" />
+                            <div className="">
+                                {showLikedBtn ? (
+                                    <i
+                                        className="fas fa-heart text-danger"
+                                        onClick={handleUnlikeTweet}
+                                    ></i>
+                                ) : (
+                                    <Like
+                                        fontSize="small"
+                                        onClick={handleLikeTweet}
+                                    />
+                                )}{" "}
+                                {likesCount > 0 && likesCount}
+                            </div>
+                            <Share />
+                        </div>
+                    )}
+                </div>
             </div>
-            {/* <Link to={slug}> */}
-            <div className="post__headerDescription" onClick={viewTweet}>
-              <p>{text}</p>
-            </div>
-          </div>
-          {tweetImage ? (
-            <img src={tweetImage} alt="" className="py-2 img-fluid" />
-          ) : null}
-
-          {/* </Link> */}
-          {token && (
-            <div className="post__footer">
-              {/* <ChatBubbleOutlineIcon fontSize="small" /> */}
-              <i className="far fa-comment"></i>
-              <RepeatIcon fontSize="small" />
-              <div className="">
-                {showLikedBtn ? (
-                  <i
-                    className="fas fa-heart text-danger"
-                    onClick={handleUnlikeTweet}
-                  ></i>
-                ) : (
-                  <FavoriteBorderIcon
-                    fontSize="small"
-                    onClick={handleLikeTweet}
-                  />
-                )}{" "}
-                {likesCount > 0 && likesCount}
-              </div>
-              <i className="fas fa-share-alt"></i>
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 );
 
 export default Tweet;
